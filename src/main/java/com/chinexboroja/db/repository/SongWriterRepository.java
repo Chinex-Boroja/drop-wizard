b1167fb9a514fb4fb2cd72cc08cbf168058246ac
Chinedu Ihedioha <ihediohachinedu21@gmail.com>
2024-01-31 14:03:11 +0100

    repo

--- src/main/java/com/chinexboroja/db/repository/SongWriterRepository.java
+++ src/main/java/com/chinexboroja/db/repository/SongWriterRepository.java
@@ -1,2 +1,26 @@
-package com.chinexboroja.db.repository;public class SongWriterRepository {
+package com.chinexboroja.db.repository;
+
+import com.chinexboroja.core.model.SongWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class SongWriterRepository {
+    private final List<SongWriter> songWriters;
+
+    public SongWriterRepository() {
+        this.songWriters = new ArrayList<>();
+    }
+    public SongWriterRepository(List<SongWriter> songWriters) {
+        this.songWriters = songWriters;
+    }
+
+    public List<SongWriter> findAll() {
+        return songWriters;
+    }
+
+    public Optional<SongWriter> getByName(final String name) {
+       return songWriters.stream().filter(sw -> sw.getName().equals(name)).findFirst();
+    }
+
 }
