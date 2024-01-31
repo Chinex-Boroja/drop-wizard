211f0e49859ff5bdd517970f797560a92fa7473a
Chinedu Ihedioha <ihediohachinedu21@gmail.com>
2024-01-31 14:03:44 +0100

    added resource

--- src/main/java/com/chinexboroja/resources/SongWriters.java
+++ src/main/java/com/chinexboroja/resources/SongWriters.java
@@ -1,2 +1,24 @@
-package com.chinexboroja.resources;public class SongWriters {
+package com.chinexboroja.resources;
+
+import com.chinexboroja.core.model.SongWriter;
+import com.chinexboroja.db.repository.SongWriterRepository;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/songwriters")
+@Produces(MediaType.APPLICATION_JSON)
+public class SongWriters {
+
+    private final SongWriterRepository repository;
+    public SongWriters(SongWriterRepository repository) {
+        this.repository = repository;
+    }
+
+    @GET
+    public List<SongWriter> getBrands() {
+        return repository.findAll();
+    }
 }
