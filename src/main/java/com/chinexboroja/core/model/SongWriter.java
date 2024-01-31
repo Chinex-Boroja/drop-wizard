05522f99807e6a06ae925f31e686ecb29f0785a9
Chinedu Ihedioha <ihediohachinedu21@gmail.com>
2024-01-31 14:02:15 +0100

    add models

--- src/main/java/com/chinexboroja/core/model/SongWriter.java
+++ src/main/java/com/chinexboroja/core/model/SongWriter.java
@@ -1,2 +1,22 @@
-package com.chinexboroja.core.model;public class SongWriter {
+package com.chinexboroja.core.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SongWriter {
+    private final String name;
+    private List<String> songs = new ArrayList<>();
+
+    public SongWriter(String name, final List<String> songs) {
+        this.name = name;
+        this.songs = songs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getSongs() {
+        return this.songs;
+    }
 }
