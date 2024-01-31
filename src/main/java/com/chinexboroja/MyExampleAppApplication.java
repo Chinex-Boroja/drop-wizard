a1490a3d3db000ecbc5ac3485679413d1e837625
Chinedu Ihedioha <ihediohachinedu21@gmail.com>
2024-01-31 14:04:11 +0100

    register with jersey

--- src/main/java/com/chinexboroja/MyExampleAppApplication.java
+++ src/main/java/com/chinexboroja/MyExampleAppApplication.java
@@ -1,11 +1,16 @@
 package com.chinexboroja;
 
+import com.chinexboroja.core.model.SongWriter;
+import com.chinexboroja.db.repository.SongWriterRepository;
 import com.chinexboroja.health.TemplateHealthCheck;
 import com.chinexboroja.resources.HelloWorldResource;
 import com.chinexboroja.resources.Message;
+import com.chinexboroja.resources.SongWriters;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyExampleAppApplication extends Application<MyExampleAppConfiguration> {
 
@@ -34,6 +39,18 @@ public class MyExampleAppApplication extends Application<MyExampleAppConfigurati
 
         TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
         environment.healthChecks().register("template", healthCheck);
+
+        //Registering the SongWriters handler
+
+        // preload songwriters
+        List<SongWriter> preload = new ArrayList<>();
+        configuration.getSongWriters().forEach(sw -> preload.add(new SongWriter(sw, new ArrayList<>())));
+        SongWriterRepository songWriterRepository = new SongWriterRepository(preload);
+        SongWriters songWriters = new SongWriters(songWriterRepository);
+        environment.jersey().register(songWriters);
+
+
+
     }
 
 }
--- src/main/java/com/chinexboroja/MyExampleAppConfiguration.java
+++ src/main/java/com/chinexboroja/MyExampleAppConfiguration.java
@@ -1,9 +1,11 @@
 package com.chinexboroja;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import io.dropwizard.core.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
 import org.hibernate.validator.constraints.*;
 import jakarta.validation.constraints.*;
 
@@ -28,5 +30,15 @@ public class MyExampleAppConfiguration extends Configuration {
     @JsonProperty
     public void setDefaultName(String name) {
         this.defaultName = name;
+    }
+
+    private List<String> songWriters;
+
+    @JsonCreator
+    public MyExampleAppConfiguration(@JsonProperty("songWriters") List<String> songWriters) {
+        this.songWriters = songWriters;
+    }
+    public List<String> getSongWriters() {
+        return this.songWriters;
     }
 }
