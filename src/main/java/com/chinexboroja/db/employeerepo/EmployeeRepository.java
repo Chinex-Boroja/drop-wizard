1a9f4cbadd5187deb74750e5e233b6de5939dd95
Chinedu Ihedioha <ihediohachinedu21@gmail.com>
2024-02-11 15:15:28 +0100

    feat: repo impl

---
+++ src/main/java/com/chinexboroja/db/employeerepo/EmployeeRepository.java
@@ -0,0 +1,33 @@
+package com.chinexboroja.db.employeerepo;
+
+import com.chinexboroja.core.model.employee.Employee;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class EmployeeRepository {
+
+    public static HashMap<Integer, Employee> map = new HashMap<>();
+
+    static {
+        map.put(1, new Employee(1, "Chinedu", "Ihedioha", "chinedu@gmail.com"));
+        map.put(2, new Employee(2, "Uzo", "Graxx", "grx@gmail.com"));
+        map.put(3, new Employee(3, "John", "Doe", "john@gmail.com"));
+    }
+
+    public static List<Employee> getEmployees() {
+        return new ArrayList<>(map.values());
+    }
+
+    public static Employee getEmployee(Integer id) {
+        return map.get(id);
+    }
+
+    public static void updateEmployee(Integer id, Employee employee) {
+        map.put(id, employee);
+    }
+
+    public static void removeEmployee(Integer id) {
+        map.remove(id);
+    }
+}
--- src/main/java/com/chinexboroja/resources/employeemanagement/EmployeeController.java
+++ src/main/java/com/chinexboroja/resources/employeemanagement/EmployeeController.java
@@ -1,4 +1,27 @@
 package com.chinexboroja.resources.employeemanagement;
 
+import com.chinexboroja.db.employeerepo.EmployeeRepository;
+import jakarta.validation.Validator;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/employees")
+@Produces(MediaType.APPLICATION_JSON)
 public class EmployeeController {
+
+    private Validator validator;
+    private EmployeeRepository repository;
+
+    public EmployeeController(Validator validator, EmployeeRepository repository) {
+        this.validator = validator;
+        this.repository = repository;
+    }
+
+    @GET
+    public Response getEmployees() {
+        return Response.ok()
+    }
 }
